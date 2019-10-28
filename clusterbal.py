3ee34a4d2d1e59099fcc560cdc80e3f3152f1cc9
Kevin Tower <ktower@uw.edu>
2019-10-28 11:27:57 -0700

    Populate hypervisors with VM data

--- clusterbal.py
+++ clusterbal.py
@@ -17,23 +17,20 @@ class osHypervisor:
 
     """A class that describes an OpenStack hypervisor and a list of all the instances running on it."""
 
-    # Class vars defined here
-    __vmDict         = {}    # Stores a dictionary of VMs assigned to the hypervisor, indexed by ID
-                             # The element contains another dict with {name, ram, vcpus, disk}
-    __name           = ""    # The name of the hypervisor
-    __hostTotalMB    = 0     # How much memory, in MB, the host has
-    __hostCurMemMB   = 0     # How much memory, in MB, the host is currently using
-    __hostNewMemMB   = 0     # How much memory, in MB, the host will be using if plan is executed
-
     def __init__(self, name, totmem, curmem):
-        self.__name = name
-        self.__hostTotalMB = totmem
-        self.__hostCurMemMB = curmem
-        self.__hostNewMemMB = curmem
+        self.__vmDict       = {}      # Stores a dictionary of VMs assigned to the hypervisor, indexed by ID
+                                      # The element contains another dict with {name, ram, vcpus, disk}
+        self.__name         = name    # The name of the hypervisor
+        self.__hostTotalMB  = totmem  # How much memory, in MB, the host has
+        self.__hostCurMemMB = curmem  # How much memory, in MB, the host is currently using
+        self.__hostNewMemMB = curmem  # How much memory, in MB, the host will be using if plan is executed
     
     def getName(self):
         """Return the hypervisor name."""
         return self.__name
+    def getNumInDict(self):
+        """Return number of objects in __vmDict.  Mostly a debug function."""
+        return len(self.__vmDict)
     def getCurPctFull(self):
         """Return a percentage of how full the hypervisor currently is."""
         return self.__hostCurMemMB / self.__hostTotalMB
@@ -110,21 +107,60 @@ class flavorCache:
             print("WARNING: Flavor {} already exists in flavor cache.  It will be overwritten.".format(flavid))
         self.__flavors[flavid] = { 'name': name, 'ram': ram, 'vcpus': vcpus, 'disk': disk, 'ephemeral': ephemeral }
 
-HypervisorDict = {} # Store osHyperVisor objects here, keyed on hypervisor hostname
+DEBUG=True      # Add extra messages?
+TOLERANCE=0.10  # How close in fullness percentage the hypervisors should be
+
+HypervisorList = [] # Store osHyperVisor objects here
 Flavors = flavorCache()
 
 def getHypervisors(cloud):
-    """Get hypervisors and add them to HypervisorDict."""
+    """Get hypervisors and add them to HypervisorList."""
     for host in cloud.list_hypervisors():
         hinfo = osHypervisor(host['hypervisor_hostname'], host['memory_mb'], host['memory_mb_used'])
-        HypervisorDict[host['hypervisor_hostname']] = hinfo
+        HypervisorList.append(hinfo)
 
+def getFlavorInfo(cloud, flavor):
+    """Given a flavor in cloud, return a tuple of data about it: [ram, vcpus, disk, ephemeral ].
+       Also use the "Flavors" global cache object to reduce the number of API calls to make for
+       flavors we already have looked up."""
+    if not Flavors.flavorExists(flavor):
+        # Need to look up the flavor via the API instead.
+        if DEBUG:
+            print("Issuing API call to look up flavor {}".format(flavor))
+        flavinfo = cloud.get_flavor(flavor)
+        fname    = flavinfo['name']
+        fram     = flavinfo['ram']
+        fvcpus   = flavinfo['vcpus']
+        fdisk    = flavinfo['disk']
+        fephem   = flavinfo['ephemeral']
+        Flavors.addFlavor(flavor, name=fname, ram=fram, vcpus=fvcpus, disk=fdisk, ephemeral=fephem)
+    
+    return [ Flavors.getFlavorResource(flavor, 'ram'),
+             Flavors.getFlavorResource(flavor, 'vcpus'),
+             Flavors.getFlavorResource(flavor, 'disk')
+            ]
+                  
 
 def main():
     """The main program here."""
     cloud = shade.OpenStackCloud()
+
+    # Get basic information about our hypervisors, and store into global list "HypervisorList"
     getHypervisors(cloud)
-    ## TODO: Populate VM data for each hypervisor
+
+    ## Populate VM data for each hypervisor
+    for hyper in HypervisorList:
+        slist=cloud.list_servers(all_projects=True, bare=True, filters={'host': hyper.getName()})
+        for s in slist:
+            sid     = s['id']
+            sname   = s['name']
+            sflavor = s['flavor']['id']
+            [ sram, svcpus, sdisk ] = getFlavorInfo(cloud, sflavor)
+            if DEBUG:
+                print("Adding {} to {}'s instance list".format(sname, hyper.getName()))
+            # Don't modify hypervisor memory; we already have a total
+            hyper.addInstance(sid, name=sname, ram=sram, vcpus=svcpus, disk=sdisk, modifymemory=False)
+
     
     ## TODO: Interatively pick random VM from most full hypervisor to move to least full
     ## TODO: Repeat until all hypervisors are within ~n percent of each other.
