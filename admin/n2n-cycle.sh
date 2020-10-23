aec9c7c17736a0a47f34a461b895d4aab920064e
Kevin Tower <ktower@uw.edu>
2020-10-23 13:41:47 -0700

    Add n2n-cycle, optimize/improve n2n-migrate

--- admin/n2n-cycle.sh
+++ admin/n2n-cycle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# Script to live-migrate all instances on a compute node to another node
+# Script to live-migrate all instances on a compute node to another node and then return
+# them to the original node.  This is used in cases where the underlying qemu binaries
+# need to be restarted, but a restart of the instance is undesirable.
 #
 # Assume the environment is already configured to work with an OpenStack environment
 
@@ -40,13 +42,15 @@ fi
 if [ $DOIT != "yes" ]; then
    echo "NOTE: Dry-run mode selected.  No migrations will occur."
 fi
-echo "This script will live migrate all instances currently assigned to $SRCHOST to $DESTHOST."
-instsondest=$(openstack server list --all-projects --host $DESTHOST -c Name -f value)
+echo "This script will live migrate all instances currently assigned to $SRCHOST to $DESTHOST"
+echo "one at a time and then migrate them back to $SRCHOST"
+# For cycling purposes, we only want running instances
+instsondest=$(openstack server list --all-projects --host $DESTHOST --status ACTIVE -c Name -f value)
 numinst=$(echo "${instsondest}" | wc -l)
 if [ $numinst -gt 0 ]; then
-   echo "WARNING: There are currently $numinst instances running on $DESTHOST:"
+   echo "NOTE: There are currently $numinst instances running on $DESTHOST:"
    echo "$instsondest"
-   echo "Please ensure you have room on $DESTHOST to accept all of ${SRCHOST}'s workloads."
+   echo "Please ensure you have room on $DESTHOST to accept the largest instance running on ${SRCHOST}."
 fi
 echo "Do you wish to continue? (Y/n)"
 read usercontinue
@@ -59,9 +63,16 @@ for inst in $(openstack server list --all-projects --host $SRCHOST -c ID -f valu
     openstack server show $inst | awk '/ name / {print $4}'
     if [ $DOIT == "yes" ]; then
        openstack server migrate --live $DESTHOST $WAITFLAG $inst
+       if [ $? -ne 0 ]; then
+           echo "WARNING: Migration of $inst was unsuccessful (RC=$?).  Not attempting to migrate back to original host."
+        else
+           openstack server migrate --live $SRCHOST $WAITFLAG $inst
+        fi
     else
        echo "Not executing: openstack server migrate --live $DESTHOST $WAITFLAG $inst"
+       echo "Not executing: openstack server migrate --live $SRCHOST $WAITFLAG $inst"
     fi
+    echo "########"
 done
 
 exit 0
